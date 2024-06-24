de10074c0583f7861e191b2269bda95f4607726d
sabil-al-haque <sabilbup2018@gmail.com>
2024-06-24 23:03:22 +0600

    Integrate HTML Template for User

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+route::get('/',[HomeController::class,'home']);
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');

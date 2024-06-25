8f8245496a4156237c055a5c23d67c4368946a9f
sabil-al-haque <sabilbup2018@gmail.com>
2024-06-25 09:23:46 +0600

    Adding Admin Template

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,17 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <H1>admin Dashboard</H1>
-    <!-- Authentication -->
-    <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <input type="submit" value="logout">
-                </form>
-</body>
+<html>
+  <head> 
+@include('admin.css')
+  </head>
+  <body>
+    <header class="header">   
+    @include('admin.header')
+    </header>
+    <div class="d-flex align-items-stretch">
+      <!-- Sidebar Navigation-->
+      @include('admin.sidebar')
+      <!-- Sidebar Navigation end-->
+      @include('admin.body')
+    </div>
+    <!-- JavaScript files-->
+    <script src="{{asset('admincss/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('admincss/vendor/popper.js/umd/popper.min.js')}}"> </script>
+    <script src="{{asset('admincss/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('admincss/vendor/jquery.cookie/jquery.cookie.js')}}"> </script>
+    <script src="{{asset('admincss/vendor/chart.js/Chart.min.js')}}"></script>
+    <script src="{{asset('admincss/vendor/jquery-validation/jquery.validate.min.js')}}"></script>
+    <script src="{{asset('admincss/js/charts-home.js')}}"></script>
+    <script src="{{asset('admincss/js/front.js')}}"></script>
+  </body>
 </html>
